e3bfb0dbe46ec008e968e7c1eb0be8b786b676da
Arjun-Meghwal <nitexam1@gmail.com>
2026-06-28 20:54:01 +0530

    Fix Gmail SMTP

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -3,26 +3,29 @@ const nodemailer = require("nodemailer");
 const mailSender = async (email, title, body) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
+      tls: {
+        rejectUnauthorized: false,
+      },
     });
 
     const info = await transporter.sendMail({
-      from: `"OTP Service" <${process.env.MAIL_USER}>`,
+      from: process.env.MAIL_USER,
       to: email,
       subject: title,
       html: body,
     });
 
-    console.log("Mail Sent Successfully");
-    console.log(info);
-    console.log("mail sent",info)
+    console.log("Mail Sent:", info.messageId);
     return info;
   } catch (error) {
-    console.log("MAIL ERROR =>", error);
+    console.error("MAIL ERROR =>", error);
     throw error;
   }
 };
